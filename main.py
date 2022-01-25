84ba20dd744b28595071c9af61dcbea3eec9ab13
William Rothan <97067057+WilliamRothan@users.noreply.github.com>
2022-01-25 23:15:40 +0000

    Bug log, desired additions log, along with added enemies, level switch bug fixes and additional enemy programming.

--- main.py
+++ main.py
@@ -1,108 +1,114 @@
 import pygame
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__()
-        self.width = 40
-        self.height = 60
-        self.image = pygame.Surface((self.width, self.height))
-        self.image.fill(PLAYER_GREEN)
-        self.rect = self.image.get_rect()
-        self.movex = 0
-        self.movey = 0
+  def __init__(self):
+    super().__init__()
+    self.width = 40
+    self.height = 60
+    self.image = pygame.Surface((self.width, self.height))
+    self.image.fill(PLAYER_GREEN)
+    self.rect = self.image.get_rect()
+    self.movex = 0
+    self.movey = 0
 
-        self.health = 15
-        self.is_colliding = False
+    self.health = 15
+    self.is_colliding = False
 
-    # Allows the player to move.
-    def control(self, x, y):
-        self.movex += x
-        self.movey += y
+  # Allows the player to move.
+  def control(self, x, y):
+    self.movex += x
+    self.movey += y
 
-    # Updates player position/stats each frame.
-    def update(self):
-        self.rect.x += self.movex
-        self.rect.y += self.movey
+  # Updates player position/stats each frame.
+  def update(self):
+    self.rect.x += self.movex
+    self.rect.y += self.movey
 
-        enemy_hit_list = pygame.sprite.spritecollide(self, enemy_list, False)
-        for enemy in enemy_hit_list:
-            self.health -= 1
-            print(self.health)
+    enemy_hit_list = pygame.sprite.spritecollide(self, enemy_list, False)
+    for enemy in enemy_hit_list:
+      self.health -= 1
+      print(self.health)
 
-        ground_hit_list = pygame.sprite.spritecollide(self, ground_list, False)
-        for ground in ground_hit_list:
-            self.movey = 0
-            self.rect.y = worldy - self.height - 30
-            self.is_colliding = True
+    ground_hit_list = pygame.sprite.spritecollide(self, ground_list, False)
+    for ground in ground_hit_list:
+      self.movey = 0
+      self.rect.y = worldy - self.height - 30
+      self.is_colliding = True
 
-        platform_hit_list = pygame.sprite.spritecollide(
-            self, platform_list, False)
-        for platform in platform_hit_list:
-            self.movey = 0
-            self.is_colliding = True
-            if self.rect.y > platform.rect.y:
-                self.rect.y = platform.rect.y + platform.rect.height
-            else:
-                self.rect.y = platform.rect.y - self.height + 1
+    platform_hit_list = pygame.sprite.spritecollide(self, platform_list, False)
+    for platform in platform_hit_list:
+      self.movey = 0
+      self.is_colliding = True
+      if self.rect.y > platform.rect.y:
+        self.rect.y = platform.rect.y + platform.rect.height
+      else:
+        self.rect.y = platform.rect.y - self.height + 1
 
-        quicksand_hit_list = pygame.sprite.spritecollide(
-            self, quicksand_list, False)
-        for quicksand in quicksand_hit_list:
-            self.movey = 0
-            self.is_colliding = True
+    quicksand_hit_list = pygame.sprite.spritecollide(self, quicksand_list, False)
+    for quicksand in quicksand_hit_list:
+      self.movey = 0
+      self.is_colliding = True
 
-    def gravity(self):
-        if self.rect.y > worldy and self.movey > 0:
-            self.movey = 0
-            self.rect.y = worldy - self.height
-        elif self.rect.y != worldy - self.height - 30:
-            self.movey += 3
+  def gravity(self):
+    if self.rect.y > worldy and self.movey > 0:
+      self.movey = 0
+      self.rect.y = worldy - self.height
+    elif self.rect.y != worldy - self.height - 30:
+      self.movey += 3
 
-    def jump(self):
-        if self.is_colliding == True:
-            self.movey -= 33
+  def jump(self):
+    if self.is_colliding == True:
+      self.movey -= 33
 
 class Enemy(pygame.sprite.Sprite):
-    def __init__(self, x, y):
-        super().__init__()
-        self.width = 40
-        self.height = 60
-        self.image = pygame.Surface([self.width, self.height])
-        self.image.fill(ENEMY_RED)
-        self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
-        self.counter = 0
-        self.is_colliding = True
+  def __init__(self, x, y):
+    super().__init__()
+    self.width = 40
+    self.height = 60
+    self.image = pygame.Surface([self.width, self.height])
+    self.image.fill(ENEMY_RED)
+    self.rect = self.image.get_rect()
+    self.rect.x = x
+    self.rect.y = y
+    self.counter = 0
+    self.is_colliding = False
 
-    def move(self):
-        distance = 80
-        speed = 4
+  def move(self):
+    distance = 80
+    speed = 4
 
-        if 0 <= self.counter <= distance:
-            self.rect.x += speed
-        elif distance <= self.counter <= distance * 2:
-            self.rect.x -= speed
-        else:
-            self.counter = 0
+    if 0 <= self.counter <= distance:
+      self.rect.x += speed
+    elif distance <= self.counter <= distance * 2:
+      self.rect.x -= speed
+    else:
+      self.counter = 0
+    
+    if self.is_colliding == False:
+      self.rect.y += 3
 
-        self.counter += 1
+    self.counter += 1
 
-        ground_hit_list = pygame.sprite.spritecollide(self, ground_list, False)
-        for ground in ground_hit_list:
-            self.movey = 0
-            self.rect.y = ground.rect.y - self.height
-            self.is_colliding = True
+    ground_hit_list = pygame.sprite.spritecollide(self, ground_list, False)
+    for ground in ground_hit_list:
+      self.movey = 0
+      self.rect.y = ground.rect.y - self.height
+      self.is_colliding = True
 
-        platform_hit_list = pygame.sprite.spritecollide(
-            self, platform_list, False)
-        for platform in platform_hit_list:
-            self.movey = 0
-            self.is_colliding = True
-            if self.rect.y > platform.rect.y:
-                self.rect.y = platform.rect.y + platform.rect.height
-            else:
-                self.rect.y = platform.rect.y - self.height + 1
+    platform_hit_list = pygame.sprite.spritecollide(self, platform_list, False)
+    for platform in platform_hit_list:
+      self.movey = 0
+      self.is_colliding = True
+      if self.rect.y > platform.rect.y:
+        self.rect.y = platform.rect.y + platform.rect.height
+      else:
+        self.rect.y = platform.rect.y - self.height + 1
+  def gravity(self):
+    if self.rect.y > worldy and self.movey > 0:
+      self.movey = 0
+      self.rect.y = worldy - self.height
+    elif self.rect.y != worldy - self.height - 30:
+      self.movey += 3
 
 
 class Platform(pygame.sprite.Sprite):
@@ -119,18 +125,6 @@ class Platform(pygame.sprite.Sprite):
 
 class Level():
     @staticmethod
-    def bad(lvl, enemy_location):
-        if lvl == 1:
-            enemy = Enemy(enemy_location[0], enemy_location[1])
-            enemy_list = pygame.sprite.Group()
-            enemy_list.add(enemy)
-        if lvl == 2:
-            enemy_list = None
-            print(f"Level {lvl}")
-
-        return enemy_list
-
-    @staticmethod
     def ground(lvl, x, y, width, height):
         ground = Platform(x, y, width, height, PLATFORM_BLACK)
 
@@ -145,6 +139,8 @@ class Level():
 """
 Setup
 """
+lvl = 1
+
 # Screen size
 worldx = 800
 worldy = 600
@@ -178,29 +174,36 @@ player.rect.y = 0
 player_list = pygame.sprite.Group()
 player_list.add(player)
 
-# Enemy position
-enemy_location = [200, 20]
-enemy_list = Level.bad(1, enemy_location)
+ground = Platform(0, 570, 800, 30, GROUND_WHITE)
 
-# Platforms
-platform1 = Platform(650, 385, 150, 125, PLATFORM_BLACK)
-platform2 = Platform(200, 78, 300, 100, PLATFORM_BLACK)
-platform3 = Platform(850, 250, 400, 50, PLATFORM_BLACK)
+# Level 1
+platform1_1 = Platform(650, 385, 150, 125, PLATFORM_BLACK)
+platform1_2 = Platform(200, 78, 300, 100, PLATFORM_BLACK)
+platform1_3 = Platform(850, 250, 400, 50, PLATFORM_BLACK)
+quicksand1_1 = Platform(400, 300, 100, 200, SAND_TAN)
+platform_list = [platform1_1, platform1_2, platform1_3]
 
-platform_list = [platform1, platform2, platform3]
+enemy1_1 = Enemy(200, 20)
+enemy1_2 = Enemy(400, 510)
+enemy_list = [enemy1_1, enemy1_2]
 
-quicksand1 = Platform(400, 300, 100, 200, SAND_TAN)
-
-ground = Platform(0, 570, 800, 30, (GROUND_WHITE))
+# Level 2
+quicksand2_1 = Platform(650, 385, 150, 125, SAND_TAN)
+quicksand2_2 = Platform(1000, 450, 250, 120, SAND_TAN)
+quicksand2_3 = Platform(300, 200, 450, 120, SAND_TAN)
 
 platform_list = pygame.sprite.Group()
-platform_list.add(platform1)
-platform_list.add(platform2)
-platform_list.add(platform3)
+platform_list.add(platform1_1)
+platform_list.add(platform1_2)
+platform_list.add(platform1_3)
 quicksand_list = pygame.sprite.Group()
-quicksand_list.add(quicksand1)
+quicksand_list.add(quicksand1_1)
 ground_list = pygame.sprite.Group()
 ground_list.add(ground)
+
+enemy_list = pygame.sprite.Group()
+enemy_list.add(enemy1_1)
+enemy_list.add(enemy1_2)
 
 # big steppy
 steps = 10
@@ -261,8 +264,7 @@ while main == True:
                 player.control(-steps, 0)
             if event.key == pygame.K_RIGHT or event.key == ord('d'):
                 player.control(steps, 0)
-            if event.key == pygame.K_UP or event.key == ord(
-                    'w') or event.key == pygame.K_SPACE:
+            if event.key == pygame.K_UP or event.key == ord('w') or event.key == pygame.K_SPACE:
                 player.jump()
             if event.key == pygame.K_DOWN or event.key == ord('s'):
                 player.height = 30
@@ -274,6 +276,48 @@ while main == True:
                 player.control(-steps, 0)
             if event.key == pygame.K_DOWN or event.key == ord('s'):
                 player.height = 60
+            if event.key == ord('l') and lvl <= 2:
+              lvl += 1
+              player.rect.x = 150
+              player.rect.y = 0
             if event.key == ord('q'):
                 pygame.quit()
                 main = False
+    
+    if lvl == 1:
+      platform_list.add(platform1_1)
+      platform_list.add(platform1_2)
+      platform_list.add(platform1_3)
+      quicksand_list.add(quicksand1_1)
+      enemy_list.add(enemy1_1)
+      enemy_list.add(enemy1_2)
+    
+    if lvl ==2:
+      platform_list.remove(platform1_1)
+      platform_list.remove(platform1_2)
+      platform_list.remove(platform1_3)
+      quicksand_list.remove(quicksand1_1)
+      enemy_list.remove(enemy1_1)
+      enemy_list.remove(enemy1_2)
+
+      quicksand_list.add(quicksand2_1)
+      quicksand_list.add(quicksand2_2)
+      quicksand_list.add(quicksand2_3)
+
+# Bugs
+# - Infinite jump
+# - Platform collision only snaps to top or bottom (no wall cling/wall jump)
+# - Crouch only shifts the sprite, it does not resize it
+# - Enemy is not affected by gravity for some reason. Also unaffected by level switching.
+# - Player has no i-frames when hit by an enemy, so their health gets melted really quickly.
+
+# Things to add
+# - Custom sprites
+# - Larger levels
+# - More than just 2 levels
+# - Proper level start/end triggers.
+# - Invisible wall at the start of the level.
+# - Different enemy types, as well as repositioning enemies in between levels
+# - Invulnerability period after the player gets hit, in which their sprite (until a proper sprite is made) will change color. This will eventually be replaced by a stagger sprite.
+# - Ways to attack/remove the enemies
+# - Other platform types (potentially things like lava, ice, etc.)
